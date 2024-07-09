2c92f5288125cfb4b8f9be53648a64d4196c2aed
evanbacon <bacon@expo.io>
2024-07-08 22:40:03 -0500

    working tests

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,28 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack, usePathname, useSegments } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { useURL } from "expo-linking";
+import { Text, Alert, View } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  console.log("Pathname>", usePathname());
+  console.log("URL>", useURL());
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -27,11 +36,55 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack>
+      </ThemeProvider>
+      <EnableURLBar />
+    </>
+  );
+}
+
+const EnableURLBar = __DEV__
+  ? () => {
+      if (useURL()?.match(/[?&]_TEST_URL=/)) {
+        return <TestURLBar />;
+      }
+      return null;
+    }
+  : () => null;
+
+// import { Screen } from "react-native-screens";
+
+function TestURLBar() {
+  const segments = useSegments();
+
+  useEffect(() => {
+    Alert.alert("URL:" + segments.join("/"), undefined, [
+      { text: "DISMISS_URL" },
+    ]);
+  }, [segments]);
+
+  return (
+    // <Screen accessible enabled>
+    <Text
+      selectable={false}
+      style={{
+        pointerEvents: "none",
+        position: "absolute",
+        zIndex: 9999,
+        top: 0,
+        left: 8,
+        right: 8,
+      }}
+      testID="EXPO_ROUTER_URL"
+    >
+      {segments.join("/")}
+    </Text>
+    // </Screen>
   );
 }
---
+++ app/modal.tsx
@@ -0,0 +1,34 @@
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet } from "react-native";
+
+export default function ModalScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <Ionicons size={310} name="code-slash" style={styles.headerImage} />
+      }
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Explore</ThemedText>
+      </ThemedView>
+      <ThemedText>Modal.</ThemedText>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
